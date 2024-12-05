6eed1229f84aee9eb95fd5dbc03aa9471578f0d1
Ayan Abdimutalip <aianabdimutalip6@gmail.com>
2024-12-05 16:03:22 +0500

    fghjkl

---
+++ script.js
@@ -0,0 +1,30 @@
+// const numbers = {a: 1, b: 2, c: 3, d: 4}
+// const {a, b, c, d} = numbers
+// console.log(a + b + c + d);
+
+
+// const obj1 =  {a: 1, b: 2}
+// const obj2 = {c: 3, d: 4}
+// const resault = {...abj1, ...obj2} 
+// console.log(resault);
+
+
+// const students = [
+//     {id: 1, name: "A"},
+//     {id: 2, name: "A"},
+//     {id: 3, name: "A"},
+//     {id: 4, name: "A"},
+//     {id: 5, name: "A"}
+// ]
+// const stuJup = students.filter((num) => num.id % 2 === 0)
+// const stuTaq = students.filter((num) => num.id % 2 !== 0)
+
+// for(let jup of stuJup){
+//     console.log("jup id " + jup.id + " " + jup.name);
+// }
+
+// for(let taq of stuTaq){
+//     console.log("taq id " + taq.id + " " + taq.name);   
+// }
+
+
